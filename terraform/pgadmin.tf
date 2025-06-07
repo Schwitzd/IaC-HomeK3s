1ddc55b2d541494185e13e0fa158ba496a70899c
Schwitzd <15292575+Schwitzd@users.noreply.github.com>
2025-06-07 10:48:25 +0200

    migrated pgadmin & postgresql to argocd

--- terraform/pgadmin.tf
+++ terraform/pgadmin.tf
@@ -1,22 +1,92 @@
+# Vault path
 data "vault_generic_secret" "pgadmin" {
   path = "${var.vault_name}/pgadmin"
 }
 
-resource "helm_release" "pgadmin" {
-  name       = "pgadmin"
-  namespace  = kubernetes_namespace.namespaces["database"].metadata[0].name
-  chart      = "pgadmin4"
-  repository = "https://helm.runix.net"
-  version    = "1.36.0"
-  cleanup_on_fail = true
+# Pgadmin Secret
+resource "kubernetes_secret" "pgadmin_secret" {
+  metadata {
+    name      = "pgadmin-secret"
+    namespace = kubernetes_namespace.namespaces["database"].metadata[0].name
+  }
 
-  values = [
-    yamlencode(yamldecode(templatefile("${path.module}/pgadmin-values.yaml", {
-      pgadmin_ingress_fqdn = "pgadmin.schwitzd.me"
-      pgadmin_email        = data.vault_generic_secret.pgadmin.data["email"]
-      pgadmin_password     = data.vault_generic_secret.pgadmin.data["password"]
-    })))
-  ]
+  data = {
+    password = data.vault_generic_secret.pgadmin.data["password"]
+  }
 
-  depends_on = [helm_release.postgresql]
+  type = "Opaque"
 }
+
+# Pgadmin Deployment 
+resource "argocd_application" "pgadmin" {
+  metadata {
+    name      = "pgadmin"
+    namespace = "infrastructure"
+  }
+
+  spec {
+    project = "database"
+
+    source {
+      repo_url        = "https://helm.runix.net"
+      chart           = "pgadmin4"
+      target_revision = "1.47.0"
+
+      helm {
+        value_files = ["$values/pgadmin/values.yaml"]
+      }
+    }
+
+    source {
+      repo_url        = argocd_repository.repos["github_gitops"].repo
+      target_revision = "HEAD"
+      ref             = "values"
+    }
+
+    destination {
+      server    = "https://kubernetes.default.svc"
+      namespace = "database"
+    }
+
+    sync_policy {
+      automated {
+        prune       = true
+        self_heal   = true
+        allow_empty = false
+      }
+
+      retry {
+        limit = 5
+        backoff {
+          duration     = "30s"
+          max_duration = "2m"
+          factor       = 2
+        }
+      }
+    }
+  }
+
+  depends_on = [
+    kubernetes_secret.pgadmin_secret,
+    argocd_project.projects["database"]
+  ]
+}
+
+## Deprecated
+#resource "helm_release" "pgadmin" {
+#  name       = "pgadmin"
+#  namespace  = kubernetes_namespace.namespaces["database"].metadata[0].name
+#  chart      = "pgadmin4"
+#  repository = "https://helm.runix.net"
+#  version    = "1.36.0"
+#  cleanup_on_fail = true
+#
+#  values = [
+#    yamlencode(yamldecode(templatefile("${path.module}/pgadmin-values.yaml", {
+#      pgadmin_ingress_fqdn = "pgadmin.schwitzd.me"
+#      pgadmin_email        = data.vault_generic_secret.pgadmin.data["email"]
+#      pgadmin_password     = data.vault_generic_secret.pgadmin.data["password"]
+#    })))
+#  ]
+#
+#}
--- terraform/postgresql.tf
+++ terraform/postgresql.tf
@@ -1,27 +1,100 @@
+# Vault path
 data "vault_generic_secret" "postgresql" {
   path = "${var.vault_name}/postgresql"
 }
 
-resource "helm_release" "postgresql" {
-  name            = "postgresql"
-  namespace       = kubernetes_namespace.namespaces["database"].metadata[0].name
-  chart           = "postgresql"
-  repository      = "oci://registry-1.docker.io/bitnamicharts"
-  version         = "16.5.5"
-  cleanup_on_fail = true
+# PostgreSQL Secrets
+resource "kubernetes_secret" "postgresql_auth" {
+  metadata {
+    name      = "postgresql-auth"
+    namespace = kubernetes_namespace.namespaces["database"].metadata[0].name
+  }
 
-  values = [
-    "${file("postgresql-values.yaml")}",
-    jsonencode({
-      global = {
-        postgresql = {
-          auth = {
-            postgresPassword = data.vault_generic_secret.postgresql.data["postgres"]
-            username         = data.vault_generic_secret.postgresql.data["username"]
-            password         = data.vault_generic_secret.postgresql.data["password"]
-          }
+  data = {
+    postgres-password = data.vault_generic_secret.postgresql.data["postgres"]
+    username          = data.vault_generic_secret.postgresql.data["username"]
+    password          = data.vault_generic_secret.postgresql.data["password"]
+  }
+
+  type = "Opaque"
+}
+
+# PostgreSQL Deployment
+resource "argocd_application" "postgresql" {
+  metadata {
+    name      = "postgresql"
+    namespace = "infrastructure"
+  }
+
+  spec {
+    project = "database"
+
+    source {
+      repo_url        = "registry-1.docker.io/bitnamicharts"
+      chart           = "postgresql"
+      target_revision = "16.7.9"
+
+      helm {
+        value_files = ["$values/postgresql/values.yaml"]
+      }
+    }
+
+    source {
+      repo_url        = argocd_repository.repos["github_gitops"].repo
+      target_revision = "HEAD"
+      ref             = "values"
+    }
+
+    destination {
+      server    = "https://kubernetes.default.svc"
+      namespace = "database"
+    }
+
+    sync_policy {
+      automated {
+        prune       = true
+        self_heal   = true
+        allow_empty = false
+      }
+
+      retry {
+        limit = 5
+        backoff {
+          duration     = "30s"
+          max_duration = "2m"
+          factor       = 2
         }
       }
-    })
+    }
+  }
+
+  depends_on = [
+    kubernetes_secret.postgresql_auth,
+    argocd_project.projects["database"]
   ]
 }
+
+## Deprecated
+#resource "helm_release" "postgresql" {
+#  name            = "postgresql"
+#  namespace       = kubernetes_namespace.namespaces["database"].metadata[0].name
+#  chart           = "postgresql"
+#  repository      = "oci://registry-1.docker.io/bitnamicharts"
+#  version         = "16.5.5"
+#  cleanup_on_fail = true
+#
+#  values = [
+#    "${file("postgresql-values.yaml")}",
+#    jsonencode({
+#      global = {
+#        postgresql = {
+#          auth = {
+#            postgresPassword = data.vault_generic_secret.postgresql.data["postgres"]
+#            username         = data.vault_generic_secret.postgresql.data["username"]
+#            password         = data.vault_generic_secret.postgresql.data["password"]
+#          }
+#        }
+#      }
+#    })
+#  ]
+#}
