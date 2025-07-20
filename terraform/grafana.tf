9e1415f658f01937a30b2a7233398ad5bc2e354e
Schwitzd <15292575+Schwitzd@users.noreply.github.com>
2025-07-20 13:29:13 +0200

    added prometheus & grafana with argocd

--- terraform/grafana.tf
+++ terraform/grafana.tf
@@ -1,22 +1,82 @@
+# Vault path
 data "vault_generic_secret" "grafana" {
   path = "${var.vault_name}/grafana"
 }
 
-resource "helm_release" "grafana" {
-  name            = "grafana"
-  namespace       = kubernetes_namespace.namespaces["monitoring"].metadata[0].name
-  chart           = "grafana"
-  repository      = "https://grafana.github.io/helm-charts"
-  version         = "8.12.1"
-  cleanup_on_fail = true
+# Grafana secret
+resource "kubernetes_secret" "grafana_admin" {
+  metadata {
+    name      = "grafana-admin-secret"
+    namespace = kubernetes_namespace.namespaces["monitoring"].metadata[0].name
+  }
 
-  values = [
-    "${file("grafana-values.yaml")}",
-    jsonencode({
-      adminUser     = data.vault_generic_secret.grafana.data["user"]
-      adminPassword = data.vault_generic_secret.grafana.data["password"]
-    })
+  data = {
+    admin-user     = data.vault_generic_secret.grafana.data["user"]
+    admin-password = data.vault_generic_secret.grafana.data["password"]
+  }
+
+  type = "Opaque"
+}
+
+# Grafama deployment
+resource "argocd_application" "grafana" {
+  metadata {
+    name      = "grafana"
+    namespace = "argocd"
+  }
+
+  spec {
+    project = "monitoring"
+
+    source {
+      repo_url        = "https://grafana.github.io/helm-charts"
+      chart           = "grafana"
+      target_revision = "9.2.10"
+
+      helm {
+        value_files = ["$values/grafana/values.yaml"]
+      }
+    }
+
+    source {
+      repo_url        = argocd_repository.repos["github_gitops"].repo
+      target_revision = "HEAD"
+      ref             = "values"
+      path            = "grafana"
+
+      directory {
+        recurse = true
+      }
+    }
+
+    destination {
+      server    = "https://kubernetes.default.svc"
+      namespace = "monitoring"
+    }
+
+    sync_policy {
+      automated {
+        prune       = true
+        self_heal   = true
+        allow_empty = false
+      }
+
+      retry {
+        limit = 5
+        backoff {
+          duration     = "30s"
+          max_duration = "2m"
+          factor       = 2
+        }
+      }
+    }
+  }
+
+  depends_on = [
+    helm_release.argocd,
+    argocd_project.projects["monitoring"],
+    kubernetes_secret.grafana_admin,
+    argocd_application.longhorn,
+    argocd_application.prometheus
   ]
-
-  depends_on = [helm_release.prometheus]
 }
--- terraform/prometheus.tf
+++ terraform/prometheus.tf
@@ -1,12 +1,67 @@
-resource "helm_release" "prometheus" {
-  name            = "prometheus"
-  namespace       = kubernetes_namespace.namespaces["monitoring"].metadata[0].name
-  repository      = "https://prometheus-community.github.io/helm-charts"
-  chart           = "prometheus"
-  version         = "27.11.0"
-  cleanup_on_fail = true
 
-  values = [
-    "${file("prometheus-values.yaml")}"
+# Prometheus deployment
+resource "argocd_application" "prometheus" {
+  metadata {
+    name      = "prometheus"
+    namespace = "argocd"
+  }
+
+  spec {
+    project = "monitoring"
+    source {
+      repo_url        = "https://prometheus-community.github.io/helm-charts"
+      chart           = "prometheus"
+      target_revision = "27.24.0"
+      helm {
+        value_files = ["$values/prometheus/values.yaml"]
+      }
+    }
+
+    source {
+      repo_url        = argocd_repository.repos["github_gitops"].repo
+      target_revision = "HEAD"
+      ref             = "values"
+    }
+
+    destination {
+      server    = "https://kubernetes.default.svc"
+      namespace = "monitoring"
+    }
+
+    sync_policy {
+      automated {
+        prune       = true
+        self_heal   = true
+        allow_empty = false
+      }
+      retry {
+        limit = 5
+        backoff {
+          duration     = "30s"
+          max_duration = "2m"
+          factor       = 2
+        }
+      }
+    }
+  }
+  depends_on = [
+    helm_release.argocd,
+    argocd_project.projects["monitoring"],
+    argocd_application.longhorn
   ]
 }
+
+
+## Deprecated
+#resource "helm_release" "prometheus" {
+#  name            = "prometheus"
+#  namespace       = kubernetes_namespace.namespaces["monitoring"].metadata[0].name
+#  repository      = "https://prometheus-community.github.io/helm-charts"
+#  chart           = "prometheus"
+#  version         = "27.11.0"
+#  cleanup_on_fail = true
+#
+#  values = [
+#    "${file("prometheus-values.yaml")}"
+#  ]
+#}
