c9fa9a2237d2a05801f3ab481e3f186ec325e335
Schwitzd <15292575+Schwitzd@users.noreply.github.com>
2025-07-27 10:34:43 +0200

    redis: migrated away from Bitnami

--- terraform/redis.tf
+++ terraform/redis.tf
@@ -4,14 +4,14 @@ data "vault_generic_secret" "redis" {
 }
 
 # Redis secret
-resource "kubernetes_secret" "redis_auth" {
+resource "kubernetes_secret" "redis" {
   metadata {
     name      = "redis-secret"
     namespace = kubernetes_namespace.namespaces["database"].metadata[0].name
   }
 
   data = {
-    redis-password = data.vault_generic_secret.redis.data["password"]
+    REDIS_PASSWORD = data.vault_generic_secret.redis.data["password"]
   }
 
   type = "Opaque"
@@ -30,9 +30,9 @@ resource "argocd_application" "redis" {
   spec {
     project = "database"
     source {
-      repo_url        = "registry-1.docker.io/bitnamicharts"
+      repo_url        = "https://groundhog2k.github.io/helm-charts/"
       chart           = "redis"
-      target_revision = "21.2.10"
+      target_revision = "2.0.3"
 
       helm {
         value_files = ["$values/redis/values.yaml"]
@@ -72,30 +72,7 @@ resource "argocd_application" "redis" {
     kubernetes_namespace.namespaces["database"],
     helm_release.argocd,
     argocd_project.projects["database"],
-    kubernetes_secret.redis_auth,
-    argocd_application.longhorn
+    kubernetes_secret.redis,
+    argocd_application.rook_ceph_cluster
   ]
 }
-
-
-## Deprecated
-#resource "helm_release" "redis" {
-#  name            = "redis"
-#  namespace       = kubernetes_namespace.namespaces["database"].metadata[0].name
-#  chart           = "redis"
-#  repository      = "oci://registry-1.docker.io/bitnamicharts"
-#  version         = "21.1.3"
-#  cleanup_on_fail = true
-#
-#
-#  values = [
-#    yamlencode(yamldecode(templatefile("redis-values.yaml", {
-#      architecture  = "standalone"
-#      storage_class = "longhorn"
-#      pvc_size      = "5Gi"
-#      redis_secret  = kubernetes_secret.redis_auth.metadata[0].name
-#    })))
-#  ]
-#
-#  depends_on = [kubernetes_secret.redis_auth]
-#}
