53e548056d00ee3613263616f00a71dfed2ef0cb
Schwitzd <15292575+Schwitzd@users.noreply.github.com>
2025-08-01 13:09:44 +0200

    harbor: updated chart

--- terraform/harbor.tf
+++ terraform/harbor.tf
@@ -6,7 +6,7 @@ data "vault_generic_secret" "harbor" {
 # Harbor secret
 resource "kubernetes_secret" "harbor_admin" {
   metadata {
-    name      = "harbor-admin-secret"
+    name      = "auth-harbor-admin"
     namespace = kubernetes_namespace.namespaces["registry"].metadata[0].name
   }
 
@@ -19,12 +19,12 @@ resource "kubernetes_secret" "harbor_admin" {
 
 resource "kubernetes_secret" "harbor_external_db" {
   metadata {
-    name      = "harbor-db-creds"
+    name      = "auth-db-harbor-db"
     namespace = kubernetes_namespace.namespaces["registry"].metadata[0].name
   }
 
   data = {
-    db-password = data.vault_generic_secret.harbor.data["db_password"]
+    password = data.vault_generic_secret.harbor.data["db_password"]
   }
 
   type = "Opaque"
@@ -32,18 +32,17 @@ resource "kubernetes_secret" "harbor_external_db" {
 
 resource "kubernetes_secret" "harbor_external_redis" {
   metadata {
-    name      = "harbor-redis-creds"
+    name      = "auth-db-harbor-redis"
     namespace = kubernetes_namespace.namespaces["registry"].metadata[0].name
   }
 
   data = {
-    redis-password = data.vault_generic_secret.redis.data["password"]
+    REDIS_PASSWORD = data.vault_generic_secret.redis.data["password"]
   }
 
   type = "Opaque"
 }
 
-# Harbor deployment
 resource "argocd_application" "harbor" {
   metadata {
     name      = "harbor"
@@ -52,7 +51,7 @@ resource "argocd_application" "harbor" {
 
   spec {
     project = "registry"
-    
+
     source {
       repo_url        = "registry-1.docker.io/bitnamicharts"
       chart           = "harbor"
@@ -60,10 +59,8 @@ resource "argocd_application" "harbor" {
 
       helm {
         value_files = ["$values/harbor/values.yaml"]
-        parameter {
-          name  = "externalDatabase.user"
-          value = data.vault_generic_secret.harbor.data["db_username"]
-        }
+
+
         parameter {
           name  = "externalRedis.password"
           value = data.vault_generic_secret.redis.data["password"]
@@ -75,6 +72,11 @@ resource "argocd_application" "harbor" {
       repo_url        = argocd_repository.repos["github_gitops"].repo
       target_revision = "HEAD"
       ref             = "values"
+      path            = "harbor"
+
+      directory {
+        recurse = true
+      }
     }
 
     destination {
@@ -106,34 +108,8 @@ resource "argocd_application" "harbor" {
     kubernetes_secret.harbor_admin,
     kubernetes_secret.harbor_external_db,
     kubernetes_secret.harbor_external_redis,
-    argocd_application.longhorn,
-    argocd_application.postgresql,
+    argocd_application.rook_ceph_cluster,
+    argocd_application.cnpg_cluster,
     argocd_application.redis
   ]
 }
-
-## Deprecated
-#resource "helm_release" "harbor" {
-#  name            = "harbor"
-#  namespace       = kubernetes_namespace.namespaces["registry"].metadata[0].name
-#  chart           = "harbor"
-#  repository      = "oci://registry-1.docker.io/bitnamicharts"
-#  version         = "25.0.2"
-#  cleanup_on_fail = true
-#
-#  values = [
-#    yamlencode(yamldecode(templatefile("harbor-values.yaml", {
-#      adminPassword              = data.vault_generic_secret.harbor.data["adminPassword"]
-#      externalURL                = "https://${data.vault_generic_secret.harbor.data["externalURL"]}"
-#      harbor_ingress_fqdn        = data.vault_generic_secret.harbor.data["externalURL"]
-#      external_database_host     = data.vault_generic_secret.harbor.data["db_hostname"]
-#      external_database_user     = data.vault_generic_secret.harbor.data["db_username"]
-#      external_database_password = data.vault_generic_secret.harbor.data["db_password"]
-#      external_redis_host        = "redis-master.database.svc.cluster.local"
-#      external_redis_port        = 6379
-#      external_redis_password    = data.vault_generic_secret.redis.data["password"]
-#    })))
-#  ]
-#
-#  depends_on = [helm_release.postgresql]
-#}
