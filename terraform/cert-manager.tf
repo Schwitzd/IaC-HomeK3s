a6cc530e195c54a06397ee791f876f428681a5e5
Schwitzd <15292575+Schwitzd@users.noreply.github.com>
2025-08-10 08:30:37 +0200

    added farm-ca

--- terraform/cert-manager.tf
+++ terraform/cert-manager.tf
@@ -4,8 +4,8 @@ data "vault_generic_secret" "cert_manager" {
 
 resource "kubernetes_secret" "cloudflare_api_token" {
   metadata {
-    name      = "cloudflare-api-token"
-    namespace = kubernetes_namespace.namespaces["infrastructure"].metadata[0].name
+    name      = "auth-api-cloudflare"
+    namespace = kubernetes_namespace.namespaces["pki"].metadata[0].name
   }
 
   type = "Opaque"
@@ -35,7 +35,7 @@ resource "kubernetes_manifest" "le_clusterissuer" {
             dns01 = {
               cloudflare = {
                 apiTokenSecretRef = {
-                  name = "cloudflare-api-token"
+                  name = "auth-api-cloudflare"
                   key  = "api-token"
                 }
               }
@@ -55,7 +55,7 @@ depends_on = [
 # cert-manager
 resource "helm_release" "cert_manager" {
   name            = "cert-manager"
-  namespace       = kubernetes_namespace.namespaces["infrastructure"].metadata[0].name
+  namespace       = kubernetes_namespace.namespaces["pki"].metadata[0].name
   repository      = "https://charts.jetstack.io"
   chart           = "cert-manager"
   version         = "1.18.2"
@@ -68,6 +68,10 @@ resource "helm_release" "cert_manager" {
       value = "true"
     }
   ]
+
+  depends_on = [ 
+    kubernetes_namespace.namespaces["kpi"]
+   ]
 }
 
 # cert-manager deployment
@@ -78,23 +82,32 @@ resource "argocd_application" "cert_manager" {
   }
 
   spec {
-    project = "infrastructure"
+    project = "pki"
 
     source {
       repo_url        = "https://charts.jetstack.io"
       chart           = "cert-manager"
       target_revision = "1.18.2"
+
+      helm {
+        value_files = ["$values/cert-manager/values.yaml"]
+      }
     }
 
     source {
       repo_url        = argocd_repository.repos["github_gitops"].repo
       target_revision = "HEAD"
       ref             = "values"
+      path            = "cert-manager"
+
+      directory {
+        recurse = true
+      }
     }
 
     destination {
       server    = "https://kubernetes.default.svc"
-      namespace = "infrastructure"
+      namespace = "pki"
     }
 
     sync_policy {
@@ -121,6 +134,59 @@ resource "argocd_application" "cert_manager" {
 
   depends_on = [
     helm_release.argocd,
-    argocd_project.projects["infrastructure"]  
+    argocd_project.projects["pki"]  
+  ]
+}
+
+# farm-ca deployment
+resource "argocd_application" "farm_ca" {
+  metadata {
+    name      = "farm-ca"
+    namespace = "argocd"
+  }
+
+  spec {
+    project = "pki"
+
+    source {
+      repo_url        = argocd_repository.repos["github_gitops"].repo
+      target_revision = "HEAD"
+      path            = "farm-ca"
+
+      directory {
+        recurse = true
+      }
+    }
+
+    destination {
+      server    = "https://kubernetes.default.svc"
+      namespace = "pki"
+    }
+
+    sync_policy {
+      automated {
+        prune       = true
+        self_heal   = true
+        allow_empty = false
+      }
+
+      retry {
+        limit = 5
+        backoff {
+          duration     = "30s"
+          max_duration = "2m"
+          factor       = 2
+        }
+      }
+
+      sync_options = [
+        "ApplyOutOfSyncOnly=true"
+      ]
+    }
+  }
+
+  depends_on = [
+    helm_release.argocd,
+    argocd_project.projects["pki"]
   ]
 }
