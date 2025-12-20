6d30f659a225a07e8c0e8929ee96fe9ae6b2ee83
Schwitzd <me@schwitzd.me>
2025-12-20 17:23:11 +0100

    added variables for azure integration with vault

--- terraform/inputs.tf
+++ terraform/inputs.tf
@@ -14,3 +14,28 @@ variable "vault_name" {
   type        = string
   sensitive   = true
 }
+
+variable "azure_location" {
+  description = "Azure region where the Key Vault and resources will be deployed"
+  type        = string
+}
+
+variable "azure_sp_vault_hc" {
+  description = "Display name for the Azure AD Service Principal used by Vault for auto-unseal"
+  type        = string
+}
+
+variable "vault_hc_rg" {
+  description = "Azure Resource Group name for the Vault auto-unseal setup"
+  type        = string
+}
+
+variable "vault_hc_key_name" {
+  description = "Azure Key Vault name for the Vault auto-unseal setup"
+  type        = string
+}
+
+variable "vault_hc_vault_key" {
+  description = "Name of the RSA key inside Azure Key Vault used by Vault for auto-unseal"
+  type        = string
+}
