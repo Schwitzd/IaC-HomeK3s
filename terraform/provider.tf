fb42af948a6592877e5011ffb555883fe8565d46
Schwitzd <me@schwitzd.me>
2025-12-20 17:22:36 +0100

    added farm vault provider

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -4,6 +4,14 @@ terraform {
       source = "hashicorp/kubernetes"
     }
 
+    azurerm = {
+      source = "hashicorp/azurerm"
+    }
+  
+    azuread = {
+      source = "hashicorp/azuread"
+    }
+
     argocd = {
       source = "argoproj-labs/argocd"
     }
@@ -26,6 +34,13 @@ provider "vault" {
   skip_child_token = true
 }
 
+provider "vault" {
+  alias            = "farm"
+  address          = data.vault_generic_secret.vault_farm.data["address"]
+  token            = data.vault_generic_secret.vault_farm.data["token"]
+  skip_child_token = true
+}
+
 provider "helm" {
   kubernetes = {
     config_path    = "~/.kube/config"
@@ -35,7 +50,7 @@ provider "helm" {
 }
 
 provider "argocd" {
-  server_addr = "argocd.schwitzd.me:443"
+  server_addr = data.vault_generic_secret.argocd.data["hostname"]
   username    = data.vault_generic_secret.argocd.data["username"]
   password    = data.vault_generic_secret.argocd.data["password"]
 }
@@ -44,4 +59,20 @@ provider "garage" {
   host   = data.vault_generic_secret.garage.data["admin_endpoint"]
   scheme = "https"
   token  = data.vault_generic_secret.garage.data["admin_token"]
+}
+
+provider "azurerm" { 
+  features {
+    key_vault {
+      purge_soft_deleted_keys_on_destroy = true
+      recover_soft_deleted_keys          = true
+    }
+  }
+
+  tenant_id       = data.vault_generic_secret.azure.data["tenant_id"]
+  subscription_id = data.vault_generic_secret.azure.data["subscription_id"]
+}
+
+provider "azuread" {
+tenant_id = data.vault_generic_secret.azure.data["tenant_id"]
 }
